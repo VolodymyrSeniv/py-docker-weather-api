a6c7d1057999e58d67913496a9990f025cfe5c9d
Seniv Volodymyr <seniv.05@gmail.com>
2025-07-30 12:33:03 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ import os
 def get_weather(key: str, city: str) -> str:
     url = "http://api.weatherapi.com/v1/current.json"
     data = requests.get(f"{url}?key={key}&q={city}")
-    city_get = data.json()["location"]["name"]
-    country = data.json()["location"]["country"]
-    time = data.json()["location"]["localtime"]
-    temp = data.json()["current"]["temp_c"]
-    text = data.json()["current"]["condition"]["text"]
-    return f"{city_get}/{country} {time} Weather: {temp}, Celsius, {text}"
+    if data.status_code==200:
+        city_get = data.json()["location"]["name"]
+        country = data.json()["location"]["country"]
+        time = data.json()["location"]["localtime"]
+        temp = data.json()["current"]["temp_c"]
+        text = data.json()["current"]["condition"]["text"]
+        return f"{city_get}/{country} {time} Weather: {temp}, Celsius, {text}"
+    return f"You got {data.status_code}."
 
 
 if __name__ == "__main__":
